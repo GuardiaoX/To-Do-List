7a624bf3219a592d4de0e86014d9a3ccea766b60
GuardiaoX <daniel.pc.cunha@gmail.com>
2026-06-16 20:16:43 -0300

    adição do html incial, só uns testes pra relembrar, muito ainda vai ser otimizado

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 import Task from './task.js'
 
 let list = []
-const task = new Task("Ler")
-saveTask(task)
+const container = document.getElementById('taskContainer');
+const button = document.getElementById('button');
+const enter = document.getElementById('input')
 
-console.log(list[0].name)
+enter.addEventListener('keydown', e => {
+    if(e.key === 'Enter'){
+        e.preventDefault();
+        button.click();
+    }
+})
 
-function saveTask(task){
-    list[list.length] = task
+
+button.addEventListener('click', e => {
+        const input = document.getElementById('input');
+
+        if(input.value != ''){
+            createTask(input.value);    
+            input.value = '';
+        }
+})
+
+
+function createTask(name){
+    const taskHolder = document.createElement('div');
+    const task = document.createElement('p');
+    const delButton = document.createElement('button');
+    
+    delButton.addEventListener('click', e=> {
+        delButton.parentElement.remove();
+    })
+
+    delButton.textContent = 'deletar'
+
+
+    task.textContent = name;
+    taskHolder.appendChild(task);
+    taskHolder.appendChild(delButton);
+    container.appendChild(taskHolder);
 }
